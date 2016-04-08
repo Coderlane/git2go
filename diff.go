3aaa2382c7e04dfc39cc024c8b063de2ccf433d8
Travis Lane <travis.lane.osx@gmail.com>
2016-04-07 21:51:00 -0700

    diff: Add DiffStats ToBuf
    This implements git_diff_stats_to_buf which provides the output for
    git diff --stats.

--- diff.go
+++ diff.go
@@ -217,6 +217,32 @@ func (stats *DiffStats) FilesChanged() int {
 	return int(C.git_diff_stats_files_changed(stats.ptr))
 }
 
+type DiffStatsFormat int
+
+const (
+	DiffStatsNone           DiffStatsFormat = C.GIT_DIFF_STATS_NONE
+	DiffStatsFull           DiffStatsFormat = C.GIT_DIFF_STATS_FULL
+	DiffStatsShort          DiffStatsFormat = C.GIT_DIFF_STATS_SHORT
+	DiffStatsNumber         DiffStatsFormat = C.GIT_DIFF_STATS_NUMBER
+	DiffStatsIncludeSummary DiffStatsFormat = C.GIT_DIFF_STATS_INCLUDE_SUMMARY
+)
+
+func (stats *DiffStats) ToBuf(format DiffStatsFormat, width uint) (string, int) {
+	buf := C.git_buf{}
+	defer C.git_buf_free(&buf)
+
+	runtime.LockOSThread()
+	defer runtime.UnlockOSThread()
+
+	ret := int(C.git_diff_stats_to_buf(&buf,
+		stats.ptr, C.git_diff_stats_format_t(format), C.size_t(width)))
+	if ret != 0 {
+		return "", ret
+	}
+
+	return C.GoString(buf.ptr), ret
+}
+
 func (diff *Diff) Stats() (*DiffStats, error) {
 	stats := new(DiffStats)
 
